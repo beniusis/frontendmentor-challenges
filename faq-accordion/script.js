4b719dd229112ed4b8d4b7a4248a79b12f0cf62b
beniusis <rokasbeniu@gmail.com>
2024-01-15 13:05:46 +0200

    fix: clickable event now only reacts if clicked on the question itself

--- faq-accordion/script.js
+++ faq-accordion/script.js
@@ -1,9 +1,9 @@
-const item = document.getElementsByClassName("accordion-item");
+const item = document.getElementsByClassName("accordion-item__question");
 const buttonIcon = document.getElementsByClassName("button-icon");
 
 for (let i = 0; i < item.length; i++) {
   item[i].addEventListener("click", () => {
-    let panel = item[i].children[1];
+    let panel = item[i].nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
       buttonIcon[i].src = "assets/images/icon-plus.svg";
