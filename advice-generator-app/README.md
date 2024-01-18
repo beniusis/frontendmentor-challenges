76e7312e1fb32414793505b9bd0c7546e3414a83
beniusis <rokasbeniu@gmail.com>
2024-01-18 18:08:16 +0200

    feat: finished advice generator app

---
+++ advice-generator-app/README.md
@@ -0,0 +1,28 @@
+# Frontend Mentor - Advice generator app solution
+
+This is a solution to the [Advice generator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/advice-generator-app-QdUG-13db). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Generate a new piece of advice by clicking the dice icon
+
+### Screenshots
+
+![Finished Desktop View](./assets/images/finished-desktop-view.png)
+
+![Finished Mobile View](./assets/images/finished-mobile-view.png)
+
+### Links
+
+- Live Site URL: [https://beniusis.github.io/frontendmentor-challenges/advice-generator-app](https://beniusis.github.io/frontendmentor-challenges/advice-generator-app)
+
+### Built with
+
+- HTML
+- CSS
+- JavaScript
+- [Advice Slip API](https://api.adviceslip.com)
---
+++ advice-generator-app/script.js
@@ -0,0 +1,28 @@
+const adviceId = document.querySelector(".advice-id");
+const adviceText = document.querySelector(".advice-text");
+const generateButton = document.querySelector(".generate-advice");
+
+generateAdvice();
+
+async function generateAdvice() {
+  try {
+    generateButton.children[0].style.animation = "spin 1s infinite";
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const advice = await response.json();
+    generateButton.children[0].style.animation = "none";
+    adviceId.textContent = advice.slip.id;
+    adviceText.textContent = advice.slip.advice;
+    generateButton.disabled = true;
+    generateButton.ariaDisabled = true;
+    await sleep(2000);
+    generateButton.disabled = false;
+    generateButton.ariaDisabled = false;
+  } catch (error) {
+    adviceId.textContent = "000";
+    adviceText.textContent = "Could not retrieve the advice. Try again.";
+  }
+}
+
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
