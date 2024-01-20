5ead91ae4825947b6835a69c404b5c6f86382200
beniusis <rokasbeniu@gmail.com>
2024-01-20 11:09:12 +0200

    docs: updated with the last completed challenge link

--- tip-calculator-app/assets/js/script.js
+++ tip-calculator-app/assets/js/script.js
@@ -9,6 +9,9 @@ const tipAmountPerPersonResult = document.querySelector(
 );
 const totalPerPersonResult = document.querySelector(".total-per-person-result");
 
+const billTotalError = document.querySelector("#bill-total-error");
+const numberOfPeopleError = document.querySelector("#number-of-people-error");
+
 const defaultTipAmountPerPerson = "$0.00";
 const defaultTotalPerPerson = "$0.00";
 
@@ -16,6 +19,7 @@ let tipAmountPerPerson;
 let totalPerPerson;
 
 setDefaultValues();
+init();
 
 function setDefaultValues() {
   tipAmountPerPersonResult.textContent = defaultTipAmountPerPerson;
@@ -26,6 +30,40 @@ function resetInputs() {
   billTotalInput.value = "";
   numberOfPeopleInput.value = "";
   customTipInput.value = "";
+}
+
+function showError(errorElement, message, element) {
+  errorElement.textContent = message;
+  errorElement.removeAttribute("hidden");
+  element.setAttribute("error", "");
+}
+
+function hideError(errorElement, element) {
+  errorElement.textContent = "";
+  errorElement.setAttribute("hidden", "");
+  element.removeAttribute("error");
+}
+
+function init() {
+  for (let i = 0; i < tips.length; i++) {
+    tips[i].addEventListener("click", () => {
+      const tipPercentage = parseInt(tips[i].value);
+      tipAmountPerPerson =
+        (parseFloat(billTotalInput.value) /
+          parseFloat(numberOfPeopleInput.value) /
+          100) *
+        tipPercentage;
+      tipAmountPerPersonResult.textContent =
+        "$" + parseFloat(tipAmountPerPerson).toFixed(2);
+      totalPerPerson =
+        parseFloat(billTotalInput.value) /
+          parseFloat(numberOfPeopleInput.value) +
+        parseFloat(tipAmountPerPerson);
+      totalPerPersonResult.textContent =
+        "$" + parseFloat(totalPerPerson).toFixed(2);
+      resetButton.removeAttribute("disabled");
+    });
+  }
 }
 
 customTipInput.addEventListener("input", () => {
@@ -54,27 +92,27 @@ customTipInput.addEventListener("input", () => {
   }
 });
 
-for (let i = 0; i < tips.length; i++) {
-  tips[i].addEventListener("click", () => {
-    const tipPercentage = parseInt(tips[i].value);
-    tipAmountPerPerson =
-      (parseFloat(billTotalInput.value) /
-        parseFloat(numberOfPeopleInput.value) /
-        100) *
-      tipPercentage;
-    tipAmountPerPersonResult.textContent =
-      "$" + parseFloat(tipAmountPerPerson).toFixed(2);
-    totalPerPerson =
-      parseFloat(billTotalInput.value) / parseFloat(numberOfPeopleInput.value) +
-      parseFloat(tipAmountPerPerson);
-    totalPerPersonResult.textContent =
-      "$" + parseFloat(totalPerPerson).toFixed(2);
-    resetButton.removeAttribute("disabled");
-  });
-}
-
 resetButton.addEventListener("click", () => {
   setDefaultValues();
   resetInputs();
   resetButton.setAttribute("disabled", "");
+});
+
+billTotalInput.addEventListener("input", () => {
+  if (billTotalInput.value === "" || parseInt(billTotalInput.value) === 0) {
+    showError(billTotalError, "Can't be zero", billTotalInput);
+  } else {
+    hideError(billTotalError, billTotalInput);
+  }
+});
+
+numberOfPeopleInput.addEventListener("input", () => {
+  if (
+    numberOfPeopleInput.value === "" ||
+    parseInt(numberOfPeopleInput.value) === 0
+  ) {
+    showError(numberOfPeopleError, "Can't be zero", numberOfPeopleInput);
+  } else {
+    hideError(numberOfPeopleError, numberOfPeopleInput);
+  }
 });
